50162cc2e4230e346c789a832142cf58f8d4aa59
TeenCoder159 <no1umang@gmail.com>
2025-04-02 01:44:07 +0800

    Add panel toggle functionality

--- src/main.js
+++ src/main.js
@@ -1,18 +1,31 @@
 const { invoke } = window.__TAURI__.core;
 
-let greetInputEl;
-let greetMsgEl;
+// let greetInputEl;
+// let greetMsgEl;
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
+// async function greet() {
+//   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+//   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+// }
+
+// window.addEventListener("DOMContentLoaded", () => {
+//   greetInputEl = document.querySelector("#greet-input");
+//   greetMsgEl = document.querySelector("#greet-msg");
+//   document.querySelector("#greet-form").addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     greet();
+//   });
+// });
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
+  document.querySelector(".icon-btn").addEventListener("click", () => {
+    var panel = document.getElementById("toggle-panel");
+    var display = getComputedStyle(panel).display;
+
+    if (display == "none") {
+      panel.style.display = "block";
+    } else {
+      panel.style.display = "none";
+    }
   });
 });
